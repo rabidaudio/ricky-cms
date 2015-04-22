6f0302dd4478aad78f5512a358b7a43b60ec722f
Charles Julian Knight <charles@rabidaudio.com>
2015-04-22 16:16:53 -0400

    allowing cross-site origin requests

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < JSONAPI::ResourceController #ActionController::Bas
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+
+  after_filter :set_access_control_headers
+
+  def set_access_control_headers 
+    headers['Access-Control-Allow-Origin'] = 'http://localhost:8000' 
+    headers['Access-Control-Request-Method'] = '*'
+  end
 end
